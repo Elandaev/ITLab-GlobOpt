daed808002f91587512cdec507fe88916a45640c
Kryukov.SA <sergey.krykov.2806@gmail.com>
2020-02-27 20:06:50 +0300

    .

--- optimization/optimization/metodLoman.cpp
+++ optimization/optimization/metodLoman.cpp
@@ -1,7 +1,77 @@
+#include "C:\Users\HP-PC\Desktop\2 семестр лабы иипр\ITLab\ITLab-GlobOpt\GlobOpt\classes.h"
 #include "metodLoman.h"
-#include<math.h>
+#include <math.h>
 #include <vector>
+
 using namespace std;
+
+void TPiyavskogoMethod::FirstIteration()
+{
+	Trials[0].x = pTask.A;
+	Trials[1].x = pTask.B;
+	Trials[0].FuncValue = func(a_);
+	Trials[0].IterationNumber = 1;
+	Trials[1].FuncValue = func(b_);
+	Trials[1].IterationNumber = 2;
+
+
+
+}
+
+double TPiyavskogoMethod::CalculateR(int p) //, double M ее вручную где то чтоли объявить?
+{
+	double R;
+	if (p == 1)
+		R= 0.5*M*(Trials[pTask.B].x - Trials[pTask.A].x) - (Trials[pTask.B].z + Trials[pTask.A].z) / 2; //добавить что за PTASK
+	else
+	R = 0.5*M*(Trials[p].x - Trials[p - 1].x) - (Trials[p].FuncValue + Trials[p - 1].FuncValue) / 2;
+
+	return R;
+
+}
+
+void TPiyavskogoMethod::CalculateIterationPoint()
+{
+
+
+
+}
+
+
+bool TPiyavskogoMethod::CheckStopCondition()
+{
+
+
+
+}
+
+bool TPiyavskogoMethod::UpdateOptimumEstimation(const TTrial& trial)
+{
+	TTrial CurTrial;
+	int t = 1;//тк имеем один интервал
+	int T = 1;//номер лучшего
+	do
+	{
+		double best_Character = CalculateR(1);
+		for (int i = 1; i < t; i++)
+		{
+	
+			if (CalculateR(i) >= best_Character)
+			{
+				best_Character = CalculateR(i);
+				T = i;
+			}
+		}
+		t++;//колич интервалов		
+		CurTrial.x = 0.5*(Trials[T].x + Trials[T - 1].x) - (Trials[T].FuncValue - Trials[T - 1].FuncValue) / (2 * m); //куда m пихнуть чтобы взять потом
+		CurTrial.FuncValue = func(CurTrial.x);
+		CurTrial.IterationNumber;
+		Trials.insert(Trials.begin() + T, CurTrial);
+	} while (CheckStopCondition()); //tmp.k < MaxTrial_ && fabs(Trials[T].x - searchInfo[T - 1].x) > eps
+
+}
+
+
 
 double func(double x)
 {
